4c0da5d9af77f63892c53082e31581e0c9f7bf99
Zyntrix026 <admin@zyntrixptvltd.com>
2026-07-21 15:35:05 +0530

    main.js issue fixed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext.jsx";
 import App from "./App.jsx";
 import "./index.css";
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider } from "react-helmet-async"
 
 createRoot(document.getElementById("root")).render(
   <HelmetProvider>
